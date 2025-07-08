90f55cf35ca1c6a9a63319f64560fe345fa14925
GustavoKasaki <gmkasaki@gmail.com>
2025-07-08 14:32:10 -0300

    feat: viewsets and tests setup

--- product/urls.py
+++ product/urls.py
@@ -5,6 +5,7 @@ from product import viewsets
 
 router = routers.SimpleRouter()
 router.register(r'products', viewsets.ProductViewSet, basename='products')
+router.register(r'categories', viewsets.CategoryViewSet, basename='categories')
 
 urlpatterns = [
     path('', include(router.urls)),
